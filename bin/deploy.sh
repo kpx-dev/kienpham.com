7a634618af69e5bc0bab7caef8a0a8b86f23275c
Kien Pham <kien@kienpham.com>
2019-11-18 00:10:32 -0800

    ci

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,39 +3,13 @@
 set -e
 set -o pipefail
 
-if [[ -n "$TOKEN" ]]; then
-	GITHUB_TOKEN=$TOKEN
-fi
-
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Set the GITHUB_TOKEN env variable."
-	exit 1
-fi
-
-if [[ -z "$TARGET_REPO" ]]; then
-	echo "Set the TARGET_REPO env variable."
-	exit 1
-fi
-
-if [[ -z "$HUGO_VERSION" ]]; then
-	HUGO_VERSION=0.59.1
-    echo 'No HUGO_VERSION was set, so defaulting to '$HUGO_VERSION
-fi
+HUGO_VERSION=0.59.1
 
 echo 'Downloading hugo'
 curl -sSL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz > /tmp/hugo.tar.gz && tar -f /tmp/hugo.tar.gz -xz
 
 echo 'Building the hugo site'
 ./hugo
-
-echo 'Cloning the GitHub Pages repo'
-BUILD_DIR=build
-rm -fr "${BUILD_DIR}"
-TARGET_REPO_URL="https://${GITHUB_TOKEN}@github.com/${TARGET_REPO}.git"
-git clone "${TARGET_REPO_URL}" "${BUILD_DIR}"
-
-echo 'Moving the content over'
-cp -r docs/* docs/
 
 echo 'Committing the site to git and pushing'
 (
@@ -46,8 +20,6 @@ echo 'Committing the site to git and pushing'
     if ! git config --get user.email; then
         git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
     fi
-
-    cd "${BUILD_DIR}"
 
     if git diff --exit-code; then
         echo "There is nothing to commit, so aborting"
