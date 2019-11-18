4bc3087b7288b06c82b7e73e132ceb10d8cc48ff
Kien Pham <kien@kienpham.com>
2019-11-18 00:56:53 -0800

    fix ci

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,23 +5,22 @@ set -o pipefail
 
 HUGO_VERSION=0.59.1
 
-echo 'Downloading hugo'
+echo "Downloading hugo v$HUGO_VERSION..."
 curl -sSL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz > /tmp/hugo.tar.gz && tar -f /tmp/hugo.tar.gz -xz
 
-echo 'Building the hugo site'
+echo 'Building the site'
 ./hugo
+
+# TODO: detect changes within /docs only
+if git diff --exit-code; then
+    echo "No diff, skipping deploy"
+    exit 0
+fi
 
 echo 'Committing the site to git and pushing'
 (
-    git config --global user.email "action@github.com"
-    git config --global user.name "Github Action"
-
-    # TODO: detect changes within /docs only
-    if git diff --exit-code; then
-        echo "There is nothing to commit, so aborting"
-        exit 0
-    fi
-
+    git config --global user.email "actions@github.com"
+    git config --global user.name "Github Actions"
     git remote rm origin
     git remote add origin https://kienpham2000:${GITHUB_TOKEN}@github.com/kienpham2000/blog.git
     git checkout -b master
