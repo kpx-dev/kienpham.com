8b45afdd8f587db854f33bf64d1d35fa0ddf699e
Kien Pham <kien@kienpham.com>
2019-11-18 00:51:45 -0800

    fix ci

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,14 +13,10 @@ echo 'Building the hugo site'
 
 echo 'Committing the site to git and pushing'
 (
-    echo "GITHUB_TOKEN2 is "
-    echo $GITHUB_TOKEN2
-
     git config --global user.email "action@github.com"
     git config --global user.name "Github Action"
-    # git config --global github.user kienpham2000
-    # git config --global github.token $GITHUB_TOKEN
 
+    # TODO: detect changes within /docs only
     # if git diff --exit-code; then
     #     echo "There is nothing to commit, so aborting"
     #     exit 0
@@ -32,22 +28,21 @@ echo 'Committing the site to git and pushing'
     git branch master
     git checkout master
 
-    echo "Adding files..."
-    git add .
+    # echo "Adding files..."
+    # git add .
 
-    echo "Creating commit..."
-    git commit -m "Publishing site $(date)"
+    # echo "Creating commit..."
+    # git commit -m "Publishing site $(date)"
 
-    echo "Git remote is"
-    git remote -v
+    # echo "Git remote is"
+    # git remote -v
 
-    echo "Pushing to repo..."
+    # echo "Pushing to repo..."
+    # git push --set-upstream origin master
+
+    git add . && \
+    git commit -m "Publishing site $(date)" && \
     git push --set-upstream origin master
-
-    # Now add all the changes and commit and push
-    # git add . && \
-    # git commit -m "Publishing site $(date)" && \
-    # git push origin master
 )
 
 echo 'Complete'
