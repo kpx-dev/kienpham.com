a84838d21991b5f0894c4d163d60ed84bfb70c35
Kien Pham <kien@kienpham.com>
2024-06-22 08:26:43 -0700

    update

--- README.md
+++ README.md
@@ -16,3 +16,10 @@ hugo server
 ```
 hugo -D
 ```
+
+## Update theme
+
+```
+git submodule add --depth=1 https://github.com/adityatelange/hugo-PaperMod.git themes/PaperMod
+git submodule update --init --recursive
+```
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-HUGO_VERSION=0.59.1
+HUGO_VERSION=0.127.0
 
 echo "Downloading hugo v$HUGO_VERSION"
 curl -sSL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz > /tmp/hugo.tar.gz && tar -f /tmp/hugo.tar.gz -xz -C /tmp
@@ -22,8 +22,8 @@ echo 'Committing the site to git and pushing'
     git config --global user.email "actions@github.com"
     git config --global user.name "Github Actions"
     git remote rm origin
-    git remote add origin https://kienpham2000:${GITHUB_TOKEN}@github.com/kienpham2000/blog.git
-    git checkout -b master
+    git remote add origin https://kpx-dev:${GITHUB_TOKEN}@github.com/kpx-dev/kienpham.com.git
+    git checkout -b main
     git add . && \
     git commit -m "Publishing site $(date)" && \
     git push --set-upstream origin master
